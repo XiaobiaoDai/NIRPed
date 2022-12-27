3a2f87dd2d33aa2b4418a240cf7865e4f604fece
XiaobiaoDai <3622@hnsyu.edu.cn>
2022-12-27 17:52:12 +0800

    a

--- keras_frcnn/__init__.py
+++ keras_frcnn/__init__.py
@@ -1,3 +1,3 @@
-#__init__.py 文件的作用是将文件夹变为一个Python模块,Python 中的每个模块的包中，都有__init__.py 文件。
-#通常__init__.py 文件为空，但是我们还可以为它增加其他的功能。我们在导入一个包时，实际上是导入了它的__init__.py文件。
-#这样我们可以在__init__.py文件中批量导入我们所需要的模块，而不再需要一个一个的导入。
+version https://git-lfs.github.com/spec/v1
+oid sha256:df74ebd2fb29dfc28513a22c78073f2424b6084d98f491939b8213218224af0c
+size 417
